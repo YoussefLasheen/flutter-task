0e031ffce1fe11c1f2a6567143f3406defb8b0ac
Youssef Lasheen <youssef@lasheen.dev>
2022-06-24 09:28:17 +0200

    Add the home screen for the app

---
+++ lib/navigation_root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/screens/home_screen.dart';
+
+class Root extends StatelessWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
